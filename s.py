f9a6ced436605223a2ca2b567a7d704a2050f16b
YashuParihar <90317738+YashuParihar@users.noreply.github.com>
2022-10-30 08:49:34 -0700

    Add files via upload

---
+++ s.py
@@ -0,0 +1,15 @@
+import os
+  
+  main1.py
+# Source file path
+source = 'main.txt'
+  
+# destination file path
+dest = 'newfile.txt'
+  
+  
+
+# using os.rename() method
+os.rename(source, dest)
+print("Source path renamed to destination path successfully.")
+
